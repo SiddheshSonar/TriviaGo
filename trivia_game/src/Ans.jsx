5fafc420319bcc846b8adbfe63db04d59fe379a1
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-03-22 21:40:54 +0530

    random options done

--- trivia_game/src/Ans.jsx
+++ trivia_game/src/Ans.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 
 function Answer(props) {
-    // console.log(props.wans)
     const answer = props.wans;
     answer.push(props.ans);
-    console.log(answer)
+    console.log(props.ans);
+    const randAns = [];
+    var j;
     if (props.type === "boolean") {
+        j = 0;
+        for (var a = [0, 1], i = a.length; i--; ) {
+            var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+            randAns[j] = answer[random]
+            j++
+        }
         return (
             <div className="answer-box">
-                <div className="answer"><div className="options">{props.ans}</div></div>
-                <div className="answer"><div className="options">{props.wans[0]}</div></div>
+                <div className="answer"><div className="options">{randAns[0]}</div></div>
+                <div className="answer"><div className="options">{randAns[1]}</div></div>
             </div>
         )
 }
     else {
+        j = 0;
+        for (var a = [0, 1, 2, 3], i = a.length; i--; ) {
+            var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+            randAns[j] = answer[random]
+            j++
+        }
         return (
             <div className="answer-box">
-                <div className="answer"><div className="options">{props.ans.replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{props.wans[0].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{props.wans[1].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{props.wans[2].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                <div className="answer"><div className="options">{randAns[0].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                <div className="answer"><div className="options">{randAns[1].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                <div className="answer"><div className="options">{randAns[2].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                <div className="answer"><div className="options">{randAns[3].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
             </div>
         )
     }
