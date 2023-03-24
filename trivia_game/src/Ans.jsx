d8dde518f39e7efd5810d79344368a895ccf83bb
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-03-24 20:02:44 +0530

    commit

--- trivia_game/src/Ans.jsx
+++ trivia_game/src/Ans.jsx
@@ -3,36 +3,69 @@ import React from "react";
 function Answer(props) {
     const answer = props.wans;
     answer.push(props.ans);
-    console.log(props.ans);
+    // console.log(props.ans);
     const randAns = [];
     var j;
+    var styles = {
+        backgroundColor: props.isChecked ? "green" : "#FFFFFF" 
+    }
+    var k = 0
+
+    React.useEffect(
+        () => {
+            console.log("test")
+            if(props.type === "boolean") { 
+            for (var a = [0, 1], i = a.length; i--; ) {
+                var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+                randAns[j] = answer[random]
+                j++
+            } 
+            console.log(randAns)
+        }
+        else {
+            for (var a = [0, 1, 2, 3], i = a.length; i--; ) {
+                var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+                randAns[j] = answer[random]
+                j++
+            }
+            console.log(randAns)
+        }
+        }
+    ,[])
     if (props.type === "boolean") {
         j = 0;
-        for (var a = [0, 1], i = a.length; i--; ) {
-            var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-            randAns[j] = answer[random]
-            j++
-        }
+        // for (var a = [0, 1], i = a.length; i--; ) {
+        //     var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+        //     randAns[j] = answer[random]
+        //     j++
+        // }
         return (
             <div className="answer-box">
-                <div className="answer"><div className="options">{randAns[0]}</div></div>
-                <div className="answer"><div className="options">{randAns[1]}</div></div>
+                 {
+                    randAns.map((ans) => (
+                        <div className="answer" style={styles} onClick={props.ansHold}><div className="options">{ans.replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                    ))
+                }
             </div>
         )
 }
     else {
-        j = 0;
-        for (var a = [0, 1, 2, 3], i = a.length; i--; ) {
-            var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-            randAns[j] = answer[random]
-            j++
-        }
+
+        // k++
+        // console.log(`rebulding  ${k}`)
+        // j = 0;
+        // for (var a = [0, 1, 2, 3], i = a.length; i--; ) {
+        //     var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+        //     randAns[j] = answer[random]
+        //     j++
+        // }
         return (
             <div className="answer-box">
-                <div className="answer"><div className="options">{randAns[0].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{randAns[1].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{randAns[2].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
-                <div className="answer"><div className="options">{randAns[3].replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                {
+                    randAns.map((ans) => (
+                        <div className="answer"  style={styles} onClick={props.ansHold}><div className="options">{ans.replace(/&#039;/g, "'").replace(/&quot;/g, '"')}</div></div>
+                    ))
+                }
             </div>
         )
     }
--- trivia_game/src/App.jsx
+++ trivia_game/src/App.jsx
@@ -9,17 +9,24 @@ import { nanoid } from 'nanoid'
 function App() {
   const [questions, setQuestions] = useState([])
   const [quest, setQuest] = useState([])
+  const [correct, setCorrect] = useState([])
 
   useEffect(() => {
       fetch("https://opentdb.com/api.php?amount=5")
       .then(res => res.json())
       .then(data => setQuestions(data.results))
+      // .then(data => console.log(data.results))
       // .then(newQuest())
   }, [])
-
+  var k = 0
 
   function generateNewQuest(i) {
-    console.log(questions[i])
+    // fetch("https://opentdb.com/api.php?amount=5")
+    //   .then(res => res.json())
+    //   .then(data => setQuestions(data.results))
+    // console.log(questions[i])
+    k++;
+    console.log(k)
     return {
       question: questions[i].question.replace(/&#039;/g, "'").replace(/&quot;/g, '"'),
       answer: questions[i].correct_answer,
@@ -27,7 +34,7 @@ function App() {
       type: questions[i].type,
       isChecked: false,
       qid: nanoid(),
-      aid: nanoid()
+      aid: nanoid(),
     }
   }
 
@@ -37,22 +44,28 @@ function App() {
     for (let i = 0; i < 5; i++) {
       newArr.push(generateNewQuest(i))
     }
-    console.log(newArr)
+    // console.log(newArr)
     setQuest(newArr)
+  }
+
+  function ansHold(id) {
+    setQuest(oldQ => oldQ.map(question => {
+      return question.aid === id ?
+      {...question, isChecked: !question.isChecked}:
+      question 
+    }))
   }
 
   const qElements = quest.map(question =>
     <div>
-      <Question key={question.qid} quest={question.question} />
-    <Answer key={question.aid} type={question.type} ans={question.answer} wans={question.options}/>
+    <Question key={question.qid} quest={question.question} />
+    <Answer key={question.aid} type={question.type} ans={question.answer} wans={question.options} isChecked ={question.isChecked} ansHold={()=>ansHold(question.aid)}/>
     </div>
   )
 
-
-
   return (
     <div className='main'>
-      <h2 className='title'>Triviago!</h2>
+      <h2 className='title'>Trivia-Go!</h2>
       <div className='question-box'>
         {qElements}
       </div>
